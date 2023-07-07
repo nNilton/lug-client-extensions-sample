63acce43c72f8aa117e3c76a85931273f457c735
Nilton Vieira <nilton.vieira@liferay.com>
2023-07-07 13:31:09 -0300

    starting custom element

--- client-extensions/scheduling-custom-element/src/App.js
+++ client-extensions/scheduling-custom-element/src/App.js
@@ -1,7 +1,34 @@
+import GlobalStyle from "./styles/global";
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import styled from "styled-components";
+import Form from "./components/Form";
+
+const Container = styled.div`
+  width:100%;
+  max-width:800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+`;
+
+const Title = styled.h2``;
+
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Title>
+          Usuários
+        </Title>
+        <Form/>
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
     </div>
   );
 }
